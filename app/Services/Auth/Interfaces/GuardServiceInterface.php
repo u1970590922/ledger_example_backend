ac88d2d5689c527598204138b1595ff71e979c64
u1970590922 <u1970590922@gmail.com>
2022-09-28 19:29:48 +0800

    增加登出訊息常數

--- app/Services/Auth/Interfaces/GuardServiceInterface.php
+++ app/Services/Auth/Interfaces/GuardServiceInterface.php
@@ -5,6 +5,13 @@ namespace App\Services\Auth\Interfaces;
 interface GuardServiceInterface
 {
     /**
+     * 登出成功訊息
+     * 
+     * @var string
+     */
+    public const LOGOUT_SUCCESS = '登出成功';
+    
+    /**
      * 將用戶登入至系統
      *
      * @param  array  $credentials [
