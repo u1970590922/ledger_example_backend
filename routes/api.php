ca35213459988be96a5c89165f476acba35403ff
u1970590922 <u1970590922@gmail.com>
2022-09-28 16:58:21 +0800

    增加登入路由

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\GuardController;
 use App\Http\Controllers\Api\Finance\LedgerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function() {
+    Route::prefix('auth')->controller(GuardController::class)->group(function() {
+        Route::post('/login', 'login');
+    });
     Route::prefix('ledger')->controller(LedgerController::class)->group(function() {
         Route::get('/', 'index');
         Route::get('/{ledger}', 'show')->where('ledger', '[0-9]+');
